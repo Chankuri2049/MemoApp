964dcb7c7a60acf58e4b45bb8374def32ede662f
Chankuri2049 <godchestnut1208@gmail.com>
2024-06-01 12:07:37 +0900

    add: メモリストのスタイリング

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,39 +4,40 @@ import React from 'react'
 const Index = (): JSX.Element => {
   return(
     <View style={style.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+
+      <View style={style.header}>
+        <View style={style.headerInner}>
+          <Text style={style.headerTitle}>Memo App</Text>
+          <Text style={style.headerRight}>ログアウト</Text>
         </View>
       </View>
 
       <View>
 
-        <View>
+        <View style={style.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
+            <Text style={style.memoListItemTitle}>買い物リスト</Text>
+            <Text style={style.memoListItemDate}>2023年10月1日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={style.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
+            <Text style={style.memoListItemTitle}>買い物リスト</Text>
+            <Text style={style.memoListItemDate}>2023年10月1日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={style.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
+            <Text style={style.memoListItemTitle}>買い物リスト</Text>
+            <Text style={style.memoListItemDate}>2023年10月1日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -54,8 +55,47 @@ const Index = (): JSX.Element => {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
   }
 })
 
